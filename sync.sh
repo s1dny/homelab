2dd4269187a62e48c15f4f1cb9349b47313d146c
s1dny <aiden@plarza.com>
2026-07-19 14:26:16 +1000

    fix: change nixos-rebuild command from boot to switch in sync script

--- sync.sh
+++ sync.sh
@@ -3,10 +3,10 @@ set -euo pipefail
 
 cd /etc/nixos
 sudo -n nix flake update homelab
-sudo -n nixos-rebuild boot --flake "/etc/nixos#$(hostname -s)"
+sudo -n nixos-rebuild switch --flake "/etc/nixos#$(hostname -s)"
 
 if ! cmp -s /etc/homelab/source/nixos/flake.nix /etc/nixos/flake.nix; then
   sudo -n homelab-sync-bootstrap
   sudo -n nix flake update
-  sudo -n nixos-rebuild boot --flake "/etc/nixos#$(hostname -s)"
+  sudo -n nixos-rebuild switch --flake "/etc/nixos#$(hostname -s)"
 fi
